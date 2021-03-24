beadcea4ed0450c533fff07cdf2fdc413b909c77
Smokey <kmboplays@gmail.com>
2021-03-24 08:55:47 +0000

    DotOS time...
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/exui_mido.mk
+    $(LOCAL_DIR)/dot_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    exui_mido-eng \
-    exui_mido-userdebug \
-    exui_mido-user
+    dot_mido-eng \
+    dot_mido-userdebug \
+    dot_mido-user
